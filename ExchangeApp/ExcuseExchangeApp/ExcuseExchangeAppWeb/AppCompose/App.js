c7bd4dfb638ab3696644a81c5c3f2024cb79aba2
Shikotan <pchelin@outlook.com>
2016-04-12 20:04:49 +0300

    Initial

---
+++ ExchangeApp/ExcuseExchangeApp/ExcuseExchangeAppWeb/AppCompose/App.js
@@ -0,0 +1,33 @@
+﻿/* Common app functionality */
+
+var app = (function () {
+    "use strict";
+
+    var app = {};
+
+    // Common initialization function (to be called from each page)
+    app.initialize = function () {
+    	$('#generate').click(function () {
+    		var name = $('#name').val();
+    		$('#put').removeAttr("disabled");
+    		$('#excuse').text(Generator.generate(name));
+    	});
+
+    	$('#put').click(function () {
+    		var textToInsert = $('#excuse').text();
+    		Office.context.mailbox.item.body.setSelectedDataAsync(
+			  textToInsert,
+			  { coercionType: Office.CoercionType.Text },
+			  function (asyncResult) {
+			  	if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
+			  		app.showNotification("Success", "\"" + textToInsert + "\" inserted successfully.");
+			  	}
+			  	else {
+			  		app.showNotification("Error", "Failed to insert \"" + textToInsert + "\": " + asyncResult.error.message);
+			  	}
+			  });
+    	});
+    };
+
+    return app;
+})();
